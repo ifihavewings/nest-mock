6406d480276f6a575e8b8c1a6f7ec0ed44cd6342
Sven <ifihavewings@163.com>
2024-05-10 09:48:04 +0800

    logger

--- src/middlewares/LoggerMiddleware.ts
+++ src/middlewares/LoggerMiddleware.ts
@@ -3,6 +3,6 @@ import { Request, Response, NextFunction } from "express";
 export class LoggerMiddleware implements NestMiddleware {
     use(req:Request, res:Response, next:NextFunction) {
         console.log('------------- LoggerMiddleware req ---------------')
-        console.log(req)
+        next()
     }
 }
